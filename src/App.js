be61d40cd4f772e0c3cbbabcd82abb462e6d3b66
Leonardo Santos de Araujo <leosantosdearaujo@gmail.com>
2022-02-15 20:54:57 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import MainProdutos from './components/MainProdutos/MainProdutos';
 class App extends React.Component {
   render() {
     return (
       <div>
-        
+        <MainProdutos></MainProdutos>
       </div>
     );
   }
